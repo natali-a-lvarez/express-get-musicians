19f6632637a708c9561df4084f37f4b755056127
natali-a-lvarez <03nataliaalvarez07@gmail.com>
2024-02-20 11:53:58 -0500

    creating get all muscicians route and test

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
 
-
-
-
-
-
+app.get("/musicians", async (req, res) => {
+  const muscicians = await Musician.findAll();
+  res.json(muscicians);
+});
 
 module.exports = app;
